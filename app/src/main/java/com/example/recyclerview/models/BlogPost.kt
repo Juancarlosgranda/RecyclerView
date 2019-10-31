be19205ad02647968a37c3571e6e63b0da7c0d4c
Juancarlos Granda <juan.granda@tecsup.edu.pe>
2019-10-31 14:45:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/models/BlogPost.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerview.models
+
+data class BlogPost(
+    var title: String,
+
+    var body: String,
+
+    var image: String,
+
+    var username: String // Author of blog post
+) {
+}
